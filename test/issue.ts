11d788e7791d5cf74ebe1d4054c39f6fe1e0dead
Matthias Rolke <mr.amtrack@gmail.com>
2023-05-08 14:05:11 +0200

    Initial commit

---
+++ test/issue.ts
@@ -0,0 +1,6 @@
+import type { Connection } from "@salesforce/core";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function reproduceIssue(conn: Connection): boolean {
+  return true;
+}
